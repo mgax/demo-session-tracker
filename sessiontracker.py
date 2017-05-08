8d2f23fbd5c46aeb73e354e3ecf4afd68925a8d1
Alex Morega <alex@grep.ro>
2017-05-08 16:23:32 +0300

    implement minimal API

--- sessiontracker.py
+++ sessiontracker.py
@@ -1,7 +1,35 @@
+import logging
 import flask
+from marshmallow import Schema, fields
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 app = flask.Flask(__name__)
 
 @app.route('/')
 def homepage():
     return 'hi'
+
+class Resolution(Schema):
+    width = fields.Int(required=True)
+    height = fields.Int(required=True)
+
+class SessionAction(Schema):
+    ip = fields.Str(required=True)
+    resolution = fields.Nested(Resolution, required=True)
+
+
+@app.route('/track/<action>', methods=['POST'])
+def track(action):
+    (info, errors) = SessionAction().load(flask.request.get_json())
+    if errors:
+        return flask.jsonify({'errors': errors}), 422
+
+    logger.info("Track: %r %r", action, info)
+
+    location = {}
+    return flask.jsonify(
+        action=action,
+        info=info,
+        location=location,
+    )
--- testsuite/test_api.py
+++ testsuite/test_api.py
@@ -21,3 +21,28 @@ def test_homepage(client):
 
     resp = requests.get('http://example.com/')
     assert resp.text == 'hi'
+
+@responses.activate
+def test_login(client):
+    def call_app(request):
+        resp = client.post(
+            '/track/login',
+            data=request.body,
+            content_type='application/json',
+        )
+        return (resp.status_code, dict(resp.headers), resp.data)
+
+    responses.add_callback(
+        responses.POST,
+        'http://example.com/track/login',
+        callback=call_app,
+    )
+
+    info = {'ip': '8.8.8.8', 'resolution': {'width': 200, 'height': 100}}
+    expected_location = {}
+    resp = requests.post('http://example.com/track/login', json=info)
+    assert resp.json() == {
+        'action': 'login',
+        'info': info,
+        'location': expected_location,
+    }
